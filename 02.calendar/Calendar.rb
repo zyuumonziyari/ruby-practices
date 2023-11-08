0fbb8c1fd9e20fb3f1fff114d709c4cd195444d2
zyuumonziyari <sou0921@fuji.waseda.jp>
2023-11-08 12:54:28 +0900

    条件分岐の簡潔化とシーケンスコードの修正、シバンの修正

--- 02.calendar/Calendar.rb
+++ 02.calendar/Calendar.rb
@@ -1,10 +1,6 @@
-#!/bin/sh
-exec ruby -x "$0" "$@"
-#!ruby
-
+#!/usr/bin/env ruby
 require 'date'
 require 'optparse'
-require 'color_echo'
 def print_calendar
   params = {}
   opt = OptionParser.new
@@ -13,26 +9,20 @@ def print_calendar
   opt.parse!(ARGV)
   requested_month = params[:m].to_i
   requested_year = params[:y].to_i 
-  case
-  when params[:m] && params[:y] && (requested_month > 12 || requested_month < 1) && (requested_year > 9999 || requested_year < 1)
-    puts "#{requested_month} is  not a valid month number (1..12)"
-    puts "#{requested_year} is not a valid year number (1..9999)"
-    return
-  when params[:y] && params[:m].nil? && (requested_year > 9999 || requested_year < 1)
-    puts "#{requested_year} is not a valid year number (1..9999)"
-    return
-  when params[:y].nil? && requested_month >= 1 && requested_month <= 12 
-    requested_year = Date.today.year 
-  when params[:m] && (requested_month > 12 || requested_month < 1)
-    puts "#{requested_month} is  not a valid month number (1..12)"
-    return
-  when params[:m].nil? && requested_year >= 1 && requested_year <= 9999
-    puts "you can specify a month by using `-m' as an option"
-    return
-  when  params[:y].nil? && params[:m].nil? 
-    requested_month = Date.today.month
-    requested_year = Date.today.year
-  end
+
+  errors = []
+  errors << "#{requested_month} is  not a valid month number (1..12)" if params[:m] && (requested_month > 12 || requested_month < 1)
+  errors << "specify the month using '-m'" if params[:m].nil? && params[:y]
+  errors << "#{requested_year} is not a valid year number (1..9999)" if params[:y] && (requested_year > 9999 || requested_year < 1)
+    if errors.length.positive?
+      puts errors
+      exit(1)
+    end
+  requested_year = Date.today.year if params[:y].nil?
+    if params[:y].nil? && params[:m].nil?
+      requested_month = Date.today.month
+      requested_year = Date.today.year
+    end
 
   first_day = Date.new(requested_year, requested_month, 1)
   last_day = Date.new(requested_year, requested_month, -1)
@@ -45,9 +35,9 @@ def print_calendar
     puts "      #{requested_month}月 #{requested_year}"  
     puts "日 月 火 水 木 金 土"
     print "   " * starting_day
-    (1..end_of_month).each do |day|
+    (first_day.day..end_of_month).each do |day|
       if requested_month == this_month && requested_year == this_year && day == thisday
-        print "\e[30m\e[47m#{day.to_s.rjust(2)}\e[0m "
+        print "\e[7m#{day.to_s.rjust(2)}\e[0m "
       else print day.to_s.rjust(2) + " "
       end
       if (day + starting_day) % 7 == 0
@@ -57,3 +47,4 @@ def print_calendar
     puts
 end
 print_calendar
+  
