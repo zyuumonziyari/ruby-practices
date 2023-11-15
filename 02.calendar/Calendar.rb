f96cc1bd20aef57efcdd4cf6b5f4ec5a9bbb37a0
zyuumonziyari <sou0921@fuji.waseda.jp>
2023-11-15 16:50:37 +0900

    条件分岐の重複解除とRangeの変更及びインデントの修正

--- 02.calendar/Calendar.rb
+++ 02.calendar/Calendar.rb
@@ -14,37 +14,35 @@ def print_calendar
   errors << "#{requested_month} is  not a valid month number (1..12)" if params[:m] && (requested_month > 12 || requested_month < 1)
   errors << "specify the month using '-m'" if params[:m].nil? && params[:y]
   errors << "#{requested_year} is not a valid year number (1..9999)" if params[:y] && (requested_year > 9999 || requested_year < 1)
-    if errors.length.positive?
-      puts errors
-      exit(1)
-    end
+  if errors.length.positive?
+    puts errors
+    exit(1)
+  end
   requested_year = Date.today.year if params[:y].nil?
-    if params[:y].nil? && params[:m].nil?
-      requested_month = Date.today.month
-      requested_year = Date.today.year
-    end
+  requested_month = Date.today.month if params[:m].nil?
 
   first_day = Date.new(requested_year, requested_month, 1)
   last_day = Date.new(requested_year, requested_month, -1)
   starting_day = first_day.wday 
   end_of_month = last_day.day
-  thisday = Date.today.day
+  today = Date.today.day
   this_month = Date.today.month.to_i
   this_year = Date.today.year.to_i
 
     puts "      #{requested_month}月 #{requested_year}"  
     puts "日 月 火 水 木 金 土"
     print "   " * starting_day
-    (first_day.day..end_of_month).each do |day|
-      if requested_month == this_month && requested_year == this_year && day == thisday
-        print "\e[7m#{day.to_s.rjust(2)}\e[0m "
-      else print day.to_s.rjust(2) + " "
+    (first_day..last_day).each do |date|
+      # ここの中では dateは各日付をあらわすDateオブジェクトになります
+      if requested_month == this_month && requested_year == this_year && date.day == today
+        print "\e[7m#{date.day.to_s.rjust(2)}\e[0m "
+      else
+        print date.day.to_s.rjust(2) + " "
       end
-      if (day + starting_day) % 7 == 0
-        puts
-      end
+      puts if date.wday == 6
     end
     puts
-end
-print_calendar
+  end
+  
+  print_calendar
   
