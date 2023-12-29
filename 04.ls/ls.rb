b8f8756429e96906dcc64e5cc69db9776861a513
zyuumonziyari <sou0921@fuji.waseda.jp>
2023-12-29 13:42:18 +0900

    引数によるメソッド間のデータ受け渡し、及び文字数の制限なくファイル名を左寄せ表示化

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,17 +3,22 @@
 COLUMNS = 3
 
 def count_current_directories
-  @current_directories = Dir.entries(Dir.pwd).reject { |entry| entry.start_with?('.') }.sort
-  @current_directories_numbers = @current_directories.count
+  current_directories = Dir.entries(Dir.pwd).reject { |entry| entry.start_with?('.') }.sort
+  current_directories_numbers = current_directories.count
+  output_current_directories(current_directories, current_directories_numbers)
 end
 
-def output_current_directories
-  max_rows = (@current_directories_numbers / COLUMNS.to_f).ceil
+def output_current_directories(current_directories, current_directories_numbers)
+  max_rows = (current_directories_numbers / COLUMNS.to_f).ceil
+  max_column_widths = (0...COLUMNS).map do |col|
+    column_values = (0...max_rows).map { |row| current_directories[row + col * max_rows] }
+    column_values.compact.map(&:length).max || 0
+  end
   max_rows.times do |row|
-    row_values = (0...COLUMNS).map { |col| @current_directories[row + col * max_rows] }.compact
-    puts row_values.map { |value| value.ljust(20) }.join
+    row_values = (0...COLUMNS).map { |col| current_directories[row + col * max_rows] }
+    formatted_row = row_values.map.with_index { |value, col| value.nil? ? '' : value.ljust(max_column_widths[col]) }
+    puts formatted_row.join(' ' * COLUMNS)
   end
 end
 
 count_current_directories
-output_current_directories
