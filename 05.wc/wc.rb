de25bd9d2f4a02fa2d49f302d32d9aa0d97e9ff7
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-02-23 23:47:05 +0900

    オプション有無の条件式を簡略化、process_filewメソッドの作成とトップレベルの処理をメソッドにに移動

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -10,11 +10,10 @@ opt.on('-c') { options[:show_byte_count] = true }
 opt.parse!(ARGV)
 file_paths = ARGV
 
-if !options[:show_line_count] && !options[:show_word_count] && !options[:show_byte_count]
-  options[:show_line_count] = true
-  options[:show_word_count] = true
-  options[:show_byte_count] = true
-end
+options.values.all?(&:!)
+options[:show_line_count] = true
+options[:show_word_count] = true
+options[:show_byte_count] = true
 
 def alert_none_file_paths
   puts 'ファイルパスを指定してください。'
@@ -23,6 +22,22 @@ end
 def process_stdin(options)
   content = $stdin.read
   output_statistics(content, options)
+end
+
+def process_files(file_paths, options)
+  total_counts = { line_count: 0, word_count: 0, byte_count: 0 }
+  file_paths.each do |file_path|
+    file_content = File.read(file_path)
+    file_counts = output_statistics(file_content, options)
+    total_counts.each_key do |key|
+      total_counts[key] += file_counts[key] if options[:"show_#{key}"]
+    end
+    print " #{file_path}\n"
+  end
+  if file_paths.length > 1
+    output_multiple_statistics(total_counts, options)
+    print '  total'
+  end
 end
 
 def output_statistics(content, options)
@@ -46,15 +61,5 @@ if !$stdin.tty?
 elsif file_paths.empty?
   alert_none_file_paths
 else
-  total_counts = { line_count: 0, word_count: 0, byte_count: 0 }
-  file_paths.each do |file_path|
-    file_content = File.read(file_path)
-    file_counts = output_statistics(file_content, options)
-    total_counts.each_key do |key|
-      total_counts[key] += file_counts[key] if options[:"show_#{key}"]
-    end
-    print " #{file_path}\n"
-  end
-  output_multiple_statistics(total_counts, options) if file_paths.length > 1
-  print '  total'
+  process_files(file_paths, options)
 end
