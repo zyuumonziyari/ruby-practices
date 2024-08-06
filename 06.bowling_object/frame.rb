1552592256ebb0605e889a3dc0fed61fedad18ef
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-06 21:59:55 +0900

    Frameクラス内で点数計算を行うように修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,23 +3,66 @@
 require_relative 'shot'
 
 class Frame
-  attr_reader :shots
+  attr_reader :shots, :frame_idx
 
-  LAST_FRAME = 10
-
-  def initialize(marks)
-    @shots = marks.map { |mark| Shot.new(mark) }
+  def initialize(frame_idx)
+    @shots = []
+    @frame_idx = frame_idx
   end
 
-  def score
-    @shots.sum(&:score)
+  def add_shot(marks)
+    loop do
+      shot = Shot.new(marks.shift)
+      @shots << shot.score
+      break if complete?
+    end
+  end
+
+  def score(next_frame, second_next_frame)
+    total_score = base_score
+    if next_frame
+      if strike?
+        total_score += strike_bonus(next_frame, second_next_frame)
+      elsif spare?
+        total_score += spare_bonus(next_frame)
+      end
+    end
+    total_score
+  end
+
+  private
+
+  def complete?
+    if @frame_idx + 1 != Game::LAST_FRAME
+      strike? || @shots.size == 2
+    else
+      @shots.size == 3 || (@shots.size == 2 && !strike? && !spare?)
+    end
+  end
+
+  def base_score
+    @shots.sum
   end
 
   def strike?
-    @shots.first.strike?
+    @shots.first == 10
   end
 
   def spare?
-    !strike? && @shots[0..1].sum(&:score) == 10
+    !strike? && @shots[0..1].sum == 10
+  end
+
+  def strike_bonus(next_frame, second_next_frame)
+    bonus = next_frame.shots.first
+    if next_frame.shots.size > 1
+      bonus += next_frame.shots[1]
+    elsif second_next_frame
+      bonus += second_next_frame.shots.first
+    end
+    bonus
+  end
+
+  def spare_bonus(next_frame)
+    next_frame.shots.first
   end
 end
