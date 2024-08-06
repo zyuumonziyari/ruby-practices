570433efd39cdef0f7ad6779efe43a23caa6d221
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-06 22:01:28 +0900

    計算処理の責務をFrameクラスに移行

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -7,60 +7,21 @@ class Game
 
   def initialize(marks)
     @frames = []
-    frame_marks = []
     frame_idx = 0
-
-    marks.each do |mark|
-      frame_marks << mark
-      if frame_idx + 1 != LAST_FRAME
-        if frame_marks[1] || frame_marks.first == 'X'
-          @frames << Frame.new(frame_marks)
-          frame_marks = []
-          frame_idx += 1
-        end
-      else
-        @frames << Frame.new(frame_marks)
-        frame_marks = []
-      end
+    while frame_idx < LAST_FRAME
+      frame = Frame.new(frame_idx)
+      frame.add_shot(marks)
+      @frames << frame
+      frame_idx += 1
     end
   end
 
   def score
-    game_score = 0
-    frame_idx = 0
-
-    @frames.each_with_index do |frame, index|
-      game_score += frame.score
-      next if frame_idx + 1 == LAST_FRAME
-
-      if frame.strike?
-        game_score += strike_bonus(index)
-      elsif frame.spare?
-        game_score += spare_bonus(index)
-      end
-      frame_idx += 1
+    @frames.sum do |frame|
+      next_frame = @frames[frame.frame_idx + 1]
+      second_next_frame = @frames[frame.frame_idx + 2]
+      frame.score(next_frame, second_next_frame)
     end
-    game_score
-  end
-
-  def strike_bonus(index)
-    bonus = 0
-    next_frame = @frames[index + 1]
-    if next_frame
-      bonus += next_frame.shots.first.score
-      if next_frame.shots[1]
-        bonus += next_frame.shots[1].score
-      else
-        second_next_frame = @frames[index + 2]
-        bonus += second_next_frame.shots.first.score if second_next_frame
-      end
-    end
-    bonus
-  end
-
-  def spare_bonus(index)
-    next_frame = @frames[index + 1]
-    next_frame ? next_frame.shots.first.score : 0
   end
 end
 
