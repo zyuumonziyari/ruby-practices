eea17021c5b41e3ba28d4095d5ac420ad6104874
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-16 17:43:01 +0900

    initializeの責務分散

--- 07.ls_object/Segment.rb
+++ 07.ls_object/Segment.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
 class Segment
-    def initialize(files)
-      @files = files.sort
-    end
-    
-    def read    
-    end
-    
-    def output
-      files = @files.reject { |entry| entry.start_with?('.') }
-      Format.new.output(files)
-    end
+  require_relative 'format'
+  
+  def initialize(option, files)
+    @option = option
+    @files = prepare_files(files)
   end
+  
+  def prepare_files(files)
+    sort_files(files)
+    filter_hidden_files(sorted_files)
+  end
+  
+  def sort_files(files)
+    @option.reverse_sort? ? files.sort.reverse : files.sort
+  end
+  
+  def filter_hidden_files(files)
+    @option.show_hidden? ? files : files.reject { |entry| entry.start_with?('.') }
+  end
+
+  def output
+    Format.new.output(@files)
+  end
+end
