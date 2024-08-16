505a5e56fd9fd2c29a1c2f5df6d13060ec4a3405
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-16 17:19:29 +0900

    メソッドの定義

--- 07.ls_object/option.rb
+++ 07.ls_object/option.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 class Option
-    require 'optparse'
+  require 'optparse'
   
-    def initialize
-      @options = { show_hidden: false, reverse_sort: false, show_long_format: false }
-      @opt = OptionParser.new
-      @opt.on('-a') { @options[:show_hidden] = true }
-      @opt.on('-r') { @options[:reverse_sort] = true }
-      @opt.on('-l') { @options[:show_long_format] = true }
-      @opt.parse!(ARGV)
-    end
-    
-    def show_hidden?
-      @options[:show_hidden]
-    end
-    
-    def reverse_sort?
-      @options[:reverse_sort]
-    end
-    
-    def show_long_format?
-      @options[:show_long_format]
-    end
+  def initialize
+    @options = { show_hidden: false, reverse_sort: false, show_long_format: false }
+    @opt = OptionParser.new
+    @opt.on('-a') { @options[:show_hidden] = true }
+    @opt.on('-r') { @options[:reverse_sort] = true }
+    @opt.on('-l') { @options[:show_long_format] = true }
+    @opt.parse!(ARGV)
   end
+  
+  def show_hidden?
+    @options[:show_hidden]
+  end
+  
+  def reverse_sort?
+    @options[:reverse_sort]
+  end
+  
+  def show_long_format?
+    @options[:show_long_format]
+  end
+end
