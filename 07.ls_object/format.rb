b9857d75932b6ab5d4bcfdf3c2b1c307ee02dfed
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-30 22:52:11 +0900

    outputの責務の明確化

--- 07.ls_object/format.rb
+++ 07.ls_object/format.rb
@@ -1,23 +1,35 @@
 # frozen_string_literal: true
 
 class Format
-#インスタンス変数がないなら、クラスにする必要はない
-#lオプションの出力のフォーマットを指定する必要はあるか？上から順に並べるだけ。
   COLUMNS = 3
 
-  #def initialize
-  #end
-
-  def output(files)
-    max_rows = (files.count / COLUMNS.to_f).ceil
-    max_column_widths = (0...COLUMNS).map do |col|
-      column_values = (0...max_rows).map { |row| files[row + col * max_rows] }
+  def initialize(segments)
+    @segments = segments
+  end
+  
+  def output    
+    format_rows.each { |row| puts row}
+  end
+  
+  private
+  
+  def calculate_max_column_widths(max_rows)
+    (0...COLUMNS).map do |col|
+      column_values = (0...max_rows).map { |row| @segments[row + col * max_rows] }
       column_values.compact.map(&:length).max || 0
     end
+  end
+  
+  def format_rows
+    max_rows = (@segments.count / COLUMNS.to_f).ceil
+    max_column_widths = calculate_max_column_widths(max_rows)
+    rows = []
+
     max_rows.times do |row|
-      row_values = (0...COLUMNS).map { |col| files[row + col * max_rows] }
+      row_values = (0...COLUMNS).map { |col| @segments[row + col * max_rows] }
       formatted_row = row_values.map.with_index { |value, col| value.nil? ? '' : value.ljust(max_column_widths[col]) }
-      puts formatted_row.join(' ' * COLUMNS)
+      rows << formatted_row.join(' ' * COLUMNS)
     end
+    rows
   end
 end
