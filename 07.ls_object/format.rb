6410e65b4fa8c661762d821a012e0a9845a12e4d
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-09-04 16:57:38 +0900

    mapの使用

--- 07.ls_object/format.rb
+++ 07.ls_object/format.rb
@@ -13,23 +13,20 @@ class Format
 
   private
 
+  def format_rows
+    max_rows = (@segments.count / COLUMNS.to_f).ceil
+    max_column_widths = calculate_max_column_widths(max_rows)
+    (0...max_rows).map do |row|
+      row_values = (0...COLUMNS).map { |col| @segments[row + col * max_rows] }
+      formatted_row = row_values.map.with_index { |value, col| value.nil? ? '' : value.ljust(max_column_widths[col]) }
+      formatted_row.join(' ' * COLUMNS)
+    end
+  end
+
   def calculate_max_column_widths(max_rows)
     (0...COLUMNS).map do |col|
       column_values = (0...max_rows).map { |row| @segments[row + col * max_rows] }
       column_values.compact.map(&:length).max || 0
     end
-  end
-
-  def format_rows
-    max_rows = (@segments.count / COLUMNS.to_f).ceil
-    max_column_widths = calculate_max_column_widths(max_rows)
-    rows = []
-
-    max_rows.times do |row|
-      row_values = (0...COLUMNS).map { |col| @segments[row + col * max_rows] }
-      formatted_row = row_values.map.with_index { |value, col| value.nil? ? '' : value.ljust(max_column_widths[col]) }
-      rows << formatted_row.join(' ' * COLUMNS)
-    end
-    rows
   end
 end
