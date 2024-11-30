afcf2228391b6bac187de9bf6de877c3f00a6f75
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-30 18:13:31 +0900

    モジュール名の修正

--- 07.ls_object/ls_command_helper.rb
+++ 07.ls_object/ls_command_helper.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 module LsCommandHelper
+  private
+  
   def filter_hidden_files(options, files)
     fileterd_files = options.show_hidden? ? files : files.reject { |entry| entry.start_with?('.') }
     sort_files(options, fileterd_files)
   end
-
+  
   def sort_files(options, files)
     options.reverse_sort? ? files.sort.reverse : files.sort
   end
 end
+
