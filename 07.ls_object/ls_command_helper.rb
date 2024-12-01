790fd737c6747763109bf488a4c581c526ff7ad5
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-12-01 19:16:24 +0900

    private化

--- 07.ls_object/ls_command_helper.rb
+++ 07.ls_object/ls_command_helper.rb
@@ -2,14 +2,13 @@
 
 module LsCommandHelper
   private
-  
+
   def filter_hidden_files(options, files)
     fileterd_files = options.show_hidden? ? files : files.reject { |entry| entry.start_with?('.') }
     sort_files(options, fileterd_files)
   end
-  
+
   def sort_files(options, files)
     options.reverse_sort? ? files.sort.reverse : files.sort
   end
 end
-
