07dda6fec17c7eef31911059318643979945bfb0
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-12-01 19:32:32 +0900

    クラスメソッドとして使用するため

--- 07.ls_object/ls_command_helper.rb
+++ 07.ls_object/ls_command_helper.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module LsCommandHelper
-  private
-
   def filter_hidden_files(options, files)
     fileterd_files = options.show_hidden? ? files : files.reject { |entry| entry.start_with?('.') }
     sort_files(options, fileterd_files)
